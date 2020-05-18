28e3cc8429d8ddbea97fce9155172b13d4e2a10c
vmanuelpm <vmanuelpm97@hotmail.com>
2020-05-18 18:28:45 -0500

    Added Kpop App youtube

---
+++ src/app/services/youtube.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { YoutubeResponse } from '../models/youtube-models';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey = 'AIzaSyCraFnvY10uETLGY8kV3c72bGnD4Z6swJg';
+  private playList = 'PLP-Bu4qV8Z71H2yyCE5p7jx-kOXpv3ZQJ';
+  private nextPageToken = '';
+
+  constructor(private http: HttpClient) {}
+   
+  getVideos() {
+
+     const url = `${this.youtubeUrl}/playlistItems`;
+     const params = new HttpParams()
+     .set('part', 'snippet')
+     .set('maxResults', '12')
+     .set('playlistId', this.playList)
+     .set('key', this.apiKey)
+     .set('pageToken', this.nextPageToken);
+
+     return this.http.get<YoutubeResponse>( url , { params } )
+     .pipe(
+      map(resp => {
+        this.nextPageToken = resp.nextPageToken;
+        return resp.items;
+      }),
+      map(items =>{
+        return items.map( video => video.snippet );
+      })
+     );
+   }
+}
